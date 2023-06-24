61179a8414fc6cdd90e2ace05d31e4fc6475e733
pacoestrada <pacoestrada77@gmail.com>
2023-06-24 11:38:51 +0200

    v0.3

--- extension.js
+++ extension.js
@@ -1,39 +1,33 @@
 const St = imports.gi.St;
 const Main = imports.ui.main;
 const PanelMenu = imports.ui.panelMenu;
-const GObject = imports.gi.GObject;
 
 let boton, botonIcono, menu;
 
-var HolaCompilando = GObject.registerClass(
-    {GTypeName: 'HolaCompilando'},
-    class HolaCompilando extends PanelMenu.Button {
-        _init() {
-            super._init(0.0, "Hola Compilando");
+var HolaCompilando = class HolaCompilando extends PanelMenu.Button {
+    _init() {
+        super._init(0.0, "Hola Compilando");
 
-            botonIcono = new St.Icon({
-                icon_name: 'system-run-symbolic',
-                style_class: 'system-status-icon'
-            });
+        botonIcono = new St.Icon({
+            icon_name: 'system-run-symbolic',
+            style_class: 'system-status-icon'
+        });
 
-            boton = new St.Bin({
-                style_class: 'panel-button',
-                reactive: true,
-                can_focus: true,
-                x_fill: true,
-                y_fill: false,
-                track_hover: true
-            });
+        boton = new St.Bin({
+            style_class: 'panel-button',
+            reactive: true,
+            can_focus: true,
+            track_hover: true
+        });
 
-            boton.set_child(botonIcono);
-            this.add_actor(boton);
+        boton.set_child(botonIcono);
+        this.add_actor(boton);
 
-            boton.connect('button-press-event', () => {
-                log("Hola Compilando");
-            });
-        }
+        boton.connect('button-press-event', () => {
+            global.log("Hola Compilando");
+        });
     }
-);
+}
 
 function init() {
 }
