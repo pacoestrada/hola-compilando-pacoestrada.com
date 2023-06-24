0966898d402e2612280df8e399d906c63486df21
pacoestrada <pacoestrada77@gmail.com>
2023-06-24 23:42:43 +0200

    cambiando página chat

--- extension.js
+++ extension.js
@@ -20,7 +20,7 @@ function init() {
 
     button.set_child(icon);
     button.connect('button-press-event', _ => {
-        Util.spawn(["google-chrome", "https://openai.com/research/chatgpt"]);
+        Util.spawn(["google-chrome", "https://chat.openai.com/auth/login"]);
     });
 }
 
