b7b8ef660f84a6edada4655117db31f1f50adfe6
pacoestrada <pacoestrada77@gmail.com>
2023-06-24 20:00:50 +0200

    v0.8

--- extension.js
+++ extension.js
@@ -1,33 +1,29 @@
 const St = imports.gi.St;
 const Main = imports.ui.main;
-const Lang = imports.lang;
 const Clutter = imports.gi.Clutter;
 const ModalDialog = imports.ui.modalDialog;
 
 let button, dialog;
 
-const MyDialog = new Lang.Class({
-    Name: 'MyDialog',
-    Extends: ModalDialog.ModalDialog,
-
-    _init: function() {
-        this.parent();
+class MyDialog extends ModalDialog.ModalDialog {
+    _init() {
+        super._init();
         let label = new St.Label({ text: 'Hola Compilando' });
         this.contentLayout.add(label);
         this.setButtons([{ label: 'Cerrar', action: this.close.bind(this), key: Clutter.KEY_Return }]);
     }
-});
+}
 
 function init() {
     button = new St.Bin({ style_class: 'panel-button' });
     let icon = new St.Icon({ icon_name: 'system-run-symbolic', style_class: 'system-status-icon' });
     button.set_child(icon);
     button.connect('button-press-event', function() {
-        dialog = new MyDialog();
+        if (!dialog) {
+            dialog = new MyDialog();
+        }
         dialog.open();
     });
-
-    dialog = new MyDialog();
 }
 
 function enable() {
@@ -36,5 +32,9 @@ function enable() {
 
 function disable() {
     Main.panel._rightBox.remove_child(button);
+    if (dialog) {
+        dialog.close();
+        dialog = null;
+    }
 }
 
